77e0178cb683c64124e120c3f10d3ddb561bfb81
peter <peter.hoppe@gmail.com>
2017-01-24 21:49:08 +0000

    Writing technical documentation.

---
+++ src/ppm_java/_dev/concept/example/multithread/wait/THouse.java
@@ -0,0 +1,63 @@
+/* -----------------------------------------------------------------------------
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+----------------------------------------------------------------------------- */
+
+package ppm_java._dev.concept.example.multithread.wait;
+
+/**
+ * @author peter
+ *
+ */
+public class THouse
+{
+    public static void main (String[] args)
+    {
+        THouse              house;
+        
+        house = new THouse ();
+        house.GetMeSomeVisitors ();
+    }
+    
+    private TVisitor[]              fVisitors;
+    
+    /**
+     * 
+     */
+    public THouse ()
+    {
+        int i;
+        
+        fVisitors = new TVisitor [10];
+        for (i = 0; i < 9; i++)
+        {
+            fVisitors[i] = new TVisitor (this, i);
+        }
+    }
+    
+    public void GetMeSomeVisitors ()
+    {
+        int i;
+        
+        for (i = 0; i < 9; i++)
+        {
+            fVisitors[i].start ();
+        }
+    }
+
+    public synchronized void Visit (int id)
+    {
+        System.out.println ("Got visitor: " + id);
+        try {Thread.sleep (1000);} catch (InterruptedException e) {}
+    }
+}
